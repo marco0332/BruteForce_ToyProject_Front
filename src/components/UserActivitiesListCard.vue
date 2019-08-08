91d26a9cba9713881182a67f24777be2bc33bb16
seongryeong <22.ryeong@gmail.com>
2019-08-08 17:57:07 +0900

    MAKE UserPage

---
+++ src/components/UserActivitiesListCard.vue
@@ -0,0 +1,182 @@
+<template>
+  <v-flex wrap xs9 style="margin-top: 5%" class="mx-auto">
+    <v-btn-toggle class="share-font">
+      <v-btn color="lime darken-1" dark small @click="selectQuestion">QUESTIONS</v-btn>
+      <v-btn color="light-green darken-1" dark small @click="selectAnswer">ANSWERS</v-btn>
+      <v-btn v-if="userId === currentUserId" color="green darken-1" dark small @click="selectComment">COMMENTS</v-btn>
+    </v-btn-toggle>
+
+    <v-card outlined v-if="question" wrap  flat width="850">
+      <v-layout column fill-height>
+        <v-list two-line>
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon color="grey lighten-2">mdi-lightbulb-off-outline</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>옴뇸뇸</v-list-item-title>
+              <v-list-item-subtitle>
+                <v-chip label color="#CFCFCF" dark small>python</v-chip>
+                <v-chip label color="#CFCFCF" dark small>django</v-chip>
+              </v-list-item-subtitle>
+            </v-list-item-content>
+            <v-spacer></v-spacer>
+            <v-list-item-content>
+              <v-list-item-subtitle><v-icon>mdi-thumb-up-outline</v-icon>&nbsp;&nbsp;&nbsp;3</v-list-item-subtitle>
+            </v-list-item-content>
+            <v-list-item-content>
+              <v-list-item-subtitle>2019. 10. 21</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+          <v-divider ></v-divider>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon color="yellow darken-2">mdi-lightbulb-on-outline</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>길게길게길게길게길게길게길게길게길게길게길게길게길게길게길게길게길게길게길게길게길게길게길게길게길게길게길게길게길게길게길게길게길게길게길게</v-list-item-title>
+              <v-list-item-subtitle>
+                <v-chip label color="#CFCFCF" dark small>python</v-chip>
+                <v-chip label color="#CFCFCF" dark small>django</v-chip>
+              </v-list-item-subtitle>
+            </v-list-item-content>
+            <v-spacer></v-spacer>
+            <v-list-item-content>
+              <v-list-item-subtitle><v-icon>mdi-thumb-up-outline</v-icon>&nbsp;&nbsp;&nbsp;3</v-list-item-subtitle>
+            </v-list-item-content>
+            <v-list-item-content>
+              <v-list-item-subtitle>2019. 10. 21</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+          <v-divider ></v-divider>
+        </v-list>
+      </v-layout>
+    </v-card>
+
+    <v-card v-if="answer" wrap  flat class="mx-auto" width="850">
+      <v-layout column fill-height>
+        <v-list two-line>
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon color="grey lighten-2">mdi-checkbox-blank-outline</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>옴뇸뇸</v-list-item-title>
+              <v-list-item-subtitle>
+                <v-chip label color="#CFCFCF" dark small>python</v-chip>
+                <v-chip label color="#CFCFCF" dark small>django</v-chip>
+              </v-list-item-subtitle>
+            </v-list-item-content>
+            <v-spacer></v-spacer>
+            <v-list-item-content>
+              <v-list-item-subtitle><v-icon>mdi-thumb-up-outline</v-icon>&nbsp;&nbsp;&nbsp;3</v-list-item-subtitle>
+            </v-list-item-content>
+            <v-list-item-content>
+              <v-list-item-subtitle>2019. 10. 21</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+          <v-divider ></v-divider>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon color="green accent-4">mdi-checkbox-marked-outline</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>길게길게길게길게길게길게길게길게길게길게길게길게길게길게길게길게길게길게길게길게길게길게길게길게길게길게길게길게길게길게길게길게길게길게길게</v-list-item-title>
+              <v-list-item-subtitle>
+                <v-chip label color="#CFCFCF" dark small>python</v-chip>
+                <v-chip label color="#CFCFCF" dark small>django</v-chip>
+              </v-list-item-subtitle>
+            </v-list-item-content>
+            <v-spacer></v-spacer>
+            <v-list-item-content>
+              <v-list-item-subtitle><v-icon>mdi-thumb-up-outline</v-icon>&nbsp;&nbsp;&nbsp;3</v-list-item-subtitle>
+            </v-list-item-content>
+            <v-list-item-content>
+              <v-list-item-subtitle>2019. 10. 21</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+          <v-divider ></v-divider>
+        </v-list>
+      </v-layout>
+    </v-card>
+
+    <v-card v-if="comment" wrap  flat class="mx-auto" width="850">
+      <v-layout column fill-height>
+        <v-list two-line>
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-format-quote-open</v-icon><v-icon>mdi-format-quote-close</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>옴뇸뇸</v-list-item-title>
+              <v-list-item-subtitle>아쉬워 벌써 목요일~~ 어떡해 벌써 목요일이야~~~</v-list-item-subtitle>
+            </v-list-item-content>
+            <v-spacer></v-spacer>
+            <v-list-item-content>
+              <v-list-item-subtitle>2019. 10. 21</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+          <v-divider ></v-divider>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-format-quote-open</v-icon><v-icon>mdi-format-quote-close</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>길게길게길게길게길게길게길게길게길게길게길게길게길게길게길게길게길게길게길게길게길게길게길게길게길게길게길게길게길게길게길게길게길게길게길게</v-list-item-title>
+              <v-list-item-subtitle>암오너봍ㅌ 눈에 보이는 사방이 바다야</v-list-item-subtitle>
+            </v-list-item-content>
+            <v-spacer></v-spacer>
+            <v-list-item-content>
+              <v-list-item-subtitle>2019. 10. 21</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+          <v-divider ></v-divider>
+        </v-list>
+      </v-layout>
+    </v-card>
+  </v-flex>
+</template>
+
+<script>
+export default {
+  name: 'UserActivitiesListCard',
+  props: {
+    userId: {type: String}
+  },
+  data () {
+    return {
+      currentUserId: '1',
+      question: true,
+      answer: false,
+      comment: false
+    }
+  },
+  methods: {
+    selectQuestion () {
+      this.question = true
+      this.answer = false
+      this.comment = false
+    },
+    selectAnswer () {
+      this.question = false
+      this.answer = true
+      this.comment = false
+    },
+    selectComment () {
+      this.question = false
+      this.answer = false
+      this.comment = true
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .share-font {
+    font-family: 'Share Tech Mono', monospace;
+    font-size: 1rem;
+  }
+</style>
--- src/views/userPage.vue
+++ src/views/userPage.vue
@@ -1,105 +1,65 @@
 <template>
   <v-app>
-    <v-container id="userPageApp" grid-list-md style="height: inherit; margin-top: 2%">
-      <v-layout wrap>
-
+    <v-container id="userPageApp" grid-list-md style="height: inherit; margin-top: 2%; margin-bottom: 2%">
+      <v-layout>
         <!--   User Information Card   -->
-        <v-flex xs3 style="margin: 2%" fixed>
-          <v-card class="mx-auto" outlined>
-            <v-img class="white--text" height="200px" :src="getImageUrl('bruteForce.png')"></v-img>
-            <v-card-title>{{ userName }}</v-card-title>
-            <v-card-text><v-icon size="17">far fa-envelope</v-icon> {{ userEmail }}<br><v-icon size="17">far fa-address-card</v-icon> {{ userGroup }}</v-card-text>
-            <v-card-text>{{ userInfo }}</v-card-text>
-            <v-divider class="mx-3 my-5"></v-divider>
+        <UserInformationCard
+          :userId="userId"
+          :userName="userName"
+          :userEmail="userEmail"
+          :userGroup="userGroup"
+          :userInfo="userInfo"
+          :userGrade="userGrade"
+          :userForce="userForce"></UserInformationCard>
 
-            <v-layout class="mr-5">
-              <v-chip  class="share-font mx-5" label size="5" :color="colors[userGrade]" text-color="white">{{grades[userGrade]}}</v-chip>
-              <v-spacer></v-spacer>
-              <span class="force">{{userForce}}</span>&nbsp;<span class="force" style="color: gray; font-size: 1rem; margin: auto;">FORCE</span>
-            </v-layout>
-            <v-card-actions>
-              <v-spacer></v-spacer>
+        <v-layout xs3></v-layout>
 
-              <!--  User Information Editor Modal  -->
-              <v-dialog v-show="currentUserId === userId" v-model="userEditor" persistent width="800">
-                <template v-slot:activator="{ on }">
-                  <v-btn icon v-on="on" @click="getUserDetailToEdit"><v-icon>fas fa-cog</v-icon></v-btn>
-                </template>
-                <v-card>
-                  <v-card-title><v-icon>fas fa-user-edit</v-icon></v-card-title>
-                  <v-card-text>
-                    <v-text-field label="User Name" v-model="editedName"></v-text-field>
-                    <v-text-field label="User Group" v-model="editedGroup"></v-text-field>
-                    <v-textarea label="User Information" v-model="editedInfo"></v-textarea>
-                  </v-card-text>
-                  <v-card-actions>
-                    <v-spacer></v-spacer>
-                    <v-btn text @click="updateUserInfo">Save</v-btn>
-                    <v-btn text @click="userEditor = false">Close</v-btn>
-                  </v-card-actions>
-                </v-card>
-              </v-dialog>
-            </v-card-actions>
-          </v-card>
-        </v-flex>
-
+        <!--  User Activity Information -->
         <v-layout xs9 style="margin-top: 2%">
-          <!--  User Activity Information -->
-          <v-flex class="share-font" wrap xs4.5 style="margin-left: 2%; margin-right: 2%">
-            <!--   Point   -->
-            <v-card outlined class="mx-auto" max-width="500">
-              <v-system-bar dark color="cyan darken-1">
-                <v-icon>fas fa-keyboard</v-icon>
-                <v-spacer></v-spacer>
-                <span class="system-bar">ACTIVITIES</span>
-              </v-system-bar>
 
-              <v-tooltip v-for="i in [0, 1, 2, 3]" :key="i" bottom>
-                <template v-slot:activator="{ on }">
-                  <v-layout wrap class="ml-1 mr-5 my-3 share-font" v-on="on" >
-                    <v-btn icon><v-icon size="32" >{{activitiesIcon[i]}}</v-icon></v-btn>
-                    <span style="color: darkgray; font-size: 1.1rem; margin: auto">{{activities[i]}}</span>
-                    <v-spacer></v-spacer>
-                    <span style="font-size: 1.8rem; margin: auto">{{userActivities[i]}}</span>
-                  </v-layout>
-                </template>
-                <span>{{tooltips[i]}}</span>
-              </v-tooltip>
-            </v-card>
-          </v-flex>
+          <!--   User Activities Number Card  -->
+          <UserActivitiesNumberCard
+            :userQuestions="userQuestions"
+            :userAnswers="userAnswers"
+            :userSelections="userSelections"
+            :userLikes="userLikes"></UserActivitiesNumberCard>
 
-          <v-flex class="share-font" wrap xs4.5 style="margin-left: 2%; margin-right: 2%">
-            <v-card outlined class="mx-auto" max-width="500">
-              <v-system-bar dark color="light-blue darken-1">
-                <v-icon>fas fa-tags</v-icon>
-                <v-spacer></v-spacer>
-                <span class="system-bar">TAGS</span>
-              </v-system-bar>
-              <v-spacer></v-spacer>
-              <v-card-text>BRUTE FORCE</v-card-text>
-              <v-card-title>
-                {{userForce}} FORCE
-              </v-card-title>
-              <v-card-text>
-
-
-              </v-card-text>
-            </v-card>
-          </v-flex>
+          <!--     Tags     -->
+          <UserTagsCard
+            :userTags="userTags"></UserTagsCard>
         </v-layout>
+      </v-layout>
+
+      <!--   Activity List    -->
+      <v-layout>
+        <v-flex xs3 style="margin: 10%"></v-flex>
+        <UserActivitiesListCard
+          :userId="userId"></UserActivitiesListCard>
       </v-layout>
     </v-container>
   </v-app>
 </template>
 
 <script>
+import UserInformationCard from '../components/UserInformationCard'
+import UserActivitiesNumberCard from '../components/UserActivitiesNumberCard'
+import UserTagsCard from '../components/UserTagsCard'
+import UserActivitiesListCard from '../components/UserActivitiesListCard'
+
 export default {
   name: 'UserPage',
   props: {
     userId: {type: String}
   },
+  components: {
+    UserActivitiesNumberCard,
+    UserInformationCard,
+    UserTagsCard,
+    UserActivitiesListCard
+  },
   data () {
     return {
+      currentUserId: '1',
       userName: 'BRUTE FORCE',
       userInfo: '안녕하세요. BRUTE FORCE 입니다.',
       userEmail: 'BRUTEFORCE@ssafy.com',
@@ -110,36 +70,11 @@ export default {
       userAnswers: 500,
       userSelections: 30,
       userLikes: 78,
-      userActivities: [],
-      editedName: '',
-      editedGroup: '',
-      editedInfo: '',
-      currentUserId: '1',
-      userEditor: false,
-      grades: ['IRON', 'BRONZE', 'SILVER', 'GOLD', 'PLATINUM', 'DIAMOND', 'MASTER', 'CHALLENGER'],
-      colors: ['#4a4433', '#c7a238', '#c7c7c7', '#fae84b', '#3e66f7', '#00fffb', '#a027d9', '#f2273f'],
-      activities: ['QUESTIONS', 'ANSWERS', 'SELECTIONS', 'LIKES'],
-      tooltips: ['User\'s Questions Number', 'User\'s Answers Number', 'User\'s Selected Answers Number', 'User\'s Liked Number'],
-      activitiesIcon: ['mdi-help', 'mdi-comment-outline', 'mdi-check', 'mdi-heart-outline']
+      userTags: ['python', 'javascript', 'C++', 'vue.js', 'vuetify', 'algorithm', 'spring', 'django'],
     }
   },
   methods: {
-    getImageUrl (img) {
-      return require('../assets/' + img)
-    },
-    getUserDetailToEdit () {
-      this.editedName = this.userName
-      this.editedGroup = this.userGroup
-      this.editedInfo = this.userInfo
-    },
-    updateUserInfo () {
-    },
-    getUserActivity () {
-      this.userActivities = [this.userQuestions, this.userAnswers, this.userSelections, this.userLikes]
-    }
-  },
-  mounted () {
-    this.getUserActivity()
+
   }
 }
 </script>
@@ -148,15 +83,8 @@ export default {
 #userPageApp {
   height: calc(100vh - 128px); /* nav_64px, footer_64px */
 }
-.system-bar{
-  font-family: 'Share Tech Mono', monospace;
-}
 .share-font {
-  font-family: 'Share Tech Mono', monospace;
-  font-size: 1rem;
-}
-.force {
-  font-family: 'Share Tech Mono', monospace;
-  font-size: 1.3rem;
-}
+   font-family: 'Share Tech Mono', monospace;
+   font-size: 1rem;
+ }
 </style>
